b62877f0c115f4a1a42ff1297276c0707aeda93b
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-07-03 09:24:38 +0800

    WIP

--- R/glm.R
+++ R/glm.R
@@ -4,50 +4,50 @@ semPlotModel.lm <- function(object, ...)
   coef <- as.matrix(coef(object))
   Nr <- nrow(coef)
   Nc <- ncol(coef)
-  
+
   combLetters <- function(x)
   {
     if (length(x)>1) return(sapply(x,combLetters))
-    
+
     f <- function(x)
-    {  
+    {
       if (x[1]>26)  c(f(floor(x/26)),x%%26 + 1) else x
     }
-    
+
     paste(LETTERS[f(x)],collapse="")
   }
-  
+
   if (is.null(rownames(coef)))
   {
-    rownames(coef) <- names(object$model)[(Nc+1):length(object$model)] 
+    rownames(coef) <- names(object$model)[(Nc+1):length(object$model)]
   }
-  
+
   if (is.null(colnames(coef)))
   {
     colnames(coef) <- names(object$model)[1:Nc]
   }
-  
+
   namesCoef <- rownames(coef)
-  stdCoef <- coef(standardize(object))
+  stdCoef <- standardized_lm(object)
   names(stdCoef) <- gsub("`","",names(stdCoef))
-  
+
   NamesR <- rownames(coef)
   NamesC <- colnames(coef)
 
-  
+
   Pars  <- data.frame(
-    label = "", 
+    label = "",
     lhs = rep(NamesR,times=Nc),
     edge = "->",
     rhs = rep(NamesC,each=Nr),
     est = c(coef),
-    std = unname(c(stdCoef[paste0(namesCoef,"s")])),
+    std = unname(c(stdCoef[namesCoef])),
     group = "",
     fixed = FALSE,
     par = 1:(Nr*Nc),
     knot = 0,
     stringsAsFactors=FALSE)
-  
+
   ## Split interactions:
   if (any(grepl(":",Pars$lhs)))
   {
@@ -64,18 +64,18 @@ semPlotModel.lm <- function(object, ...)
       }
     }
   }
-  
+
   Pars$edge[grepl("intercept",Pars$lhs,ignore.case=TRUE)] <- "int"
   Pars$lhs[grepl("intercept",Pars$lhs,ignore.case=TRUE)] <- ""
-  
-  # Variable dataframe: 
+
+  # Variable dataframe:
   Vars <- data.frame(
     name = unique(c(Pars$lhs,Pars$rhs)),
     manifest = TRUE,
     exogenous = NA,
     stringsAsFactors=FALSE)
   Vars <- Vars[Vars$name!="",]
-  
+
   semModel <- new("semPlotModel")
   semModel@Pars <- Pars
   semModel@Vars <- Vars
@@ -83,6 +83,32 @@ semPlotModel.lm <- function(object, ...)
   semModel@Original <- list(object)
   semModel@ObsCovs <- list()
   semModel@ImpCovs <- list()
-  
+
   return(semModel)
+}
+
+standardized_lm <- function(x) {
+  mf <- stats::model.frame(x)
+  tt <- stats::terms(x)
+  y <- mf[, attr(tt, "response")]
+  has_intercept <- (attr(tt, "intercept") != 0)
+  if (has_intercept) {
+      xx <- stats::model.matrix(x)[, -1]
+      b <- stats::coef(x)[-1]
+    } else {
+      xx <- stats::model.matrix(x)
+      b <- stats::coef(x)
+    }
+  y_sd <- sd(y)
+  xx_sd <- apply(xx, 2, sd)
+  beta <- b * xx_sd / y_sd
+  if (has_intercept) {
+      tnames <- names(b)[-1]
+    } else {
+      tnames <- names(b)
+    }
+  if (!identical(names(beta), names(b))) {
+      stop("Something's wrong. The terms do not match.")
+    }
+  return(beta)
 }
